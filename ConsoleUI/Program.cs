728bd80a18bbd5d05ed6cd912e11189dcf6189eb
Kutay Tıstık <kutaytistik99@gmail.com>
2023-04-26 23:35:39 +0300

    26.04.2023

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerDal customerDal = new EfCustomerDal();
+            CustomerManager customerManager = new CustomerManager(customerDal);
+            var result = customerManager.GetCustomerDetail();
+            if (result.Success)
+            {
+                foreach (var customer in result.Data)
+                {
+                    Console.WriteLine(customer.FirstName + " " + customer.LastName + " - " + customer.CompanyName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
+            
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
